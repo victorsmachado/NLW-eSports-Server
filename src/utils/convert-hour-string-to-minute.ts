57988361fb4a3edf757e6ee8e69e156731aabcc3
victorsmachado <48452755+victorsmachado@users.noreply.github.com>
2022-09-16 09:56:08 -0300

    first commit

---
+++ src/utils/convert-hour-string-to-minute.ts
@@ -0,0 +1,11 @@
+
+// 18:00 -> ['18', '00'] -> [18, 00]
+
+
+export function convertHourStringToMinute(hourString: string){
+    const[hours, minutes] = hourString.split(':').map(Number);
+
+    const minutesAmount = (hours * 60 + minutes);
+
+    return minutesAmount;
+}
